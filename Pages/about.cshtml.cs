ecedb60965e06196348f00de94af5c785627e0a7
Christy Hartner <xtyvkv@gmail.com>
2022-06-30 23:14:43 -0400

    Initial commit

---
+++ Pages/about.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Ahbc2022AprilWebApp1.Pages
+{
+    public class aboutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
